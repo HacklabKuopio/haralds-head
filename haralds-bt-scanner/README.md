13156ac57c5f84859b6ccb863a9d439ab33ca777
mika <142736820+HacklabKuopio@users.noreply.github.com>
2024-03-03 19:02:39 +0200

    add BT scanner

---
+++ haralds-bt-scanner/README.md
@@ -0,0 +1,4 @@
+## Harald's BT Scanner
+
+Stand-alone Python script to scan visible Bluetooth devices and write their name and MAC onto a scanlog file
+and POST each item as json to a listening HTTP server. This does not attempt to pair or connect to any device.
---
+++ haralds-bt-scanner/harald-bt-scan.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+import bluetooth
+from bluetooth.ble import DiscoveryService
+#from bluepy.btle import Scanner, DefaultDelegate, BTLEDisconnectError
+from optparse import OptionParser
+import requests
+import time
+import threading
+import logging
+import re
+
+HARALDS_FLASK_SERVER = "http://192.168.10.1:5000"
+
+logger = logging.getLogger("btscan")
+
+
+class HaraldsBTScanner(object):
+    def __init__(self, timeout):
+        self.classic_scanner = HaraldsBTScanner.ClassicScanner(timeout)
+        self.ble_scanner = HaraldsBTScanner.BLEScanner(timeout)
+
+    def start(self):
+        logger.info("Starting Harald Scanner")
+        # scanning concurrently does not discover all devices...
+        while True:
+            self.classic_scanner.scan()
+            #self.classic_scanner.join()
+
+            self.ble_scanner.scan()
+            #self.ble_scanner.join()
+
+
+    class ClassicScanner(threading.Thread):
+        def __init__(self, timeout, *args):
+            threading.Thread.__init__(self)
+            self.timeout = timeout
+
+        def scan(self):
+            logger.info("\x1b[34;20mScanning classic...\x1b[0m")
+            try:
+                devices = bluetooth.discover_devices(duration=self.timeout, lookup_names=True)
+                for addr, name in devices:
+                    # RSSI is not directly available for classic Bluetooth in PyBluez
+                    rssi = "N/A"
+                    logger.info(f"Classic - Name: {name}, MAC: {addr}")
+                    HaraldsBTScanner.store_scanlog({"name": name, "addr": addr})
+                #time.sleep(self.timeout)
+            except Exception as e:
+                logger.warn(f"Error retrieving classic Bluetooth device info: {e}")
+                time.sleep(2)
+
+
+    class BLEScanner(threading.Thread):
+        def __init__(self, timeout, *args):
+            threading.Thread.__init__(self)
+            self.timeout = timeout
+            self.service = DiscoveryService()
+
+        def scan(self):
+            logger.info("\x1b[34;20mScanning BLE...\x1b[0m")
+            try:
+                #time.sleep(self.timeout)
+                devices = self.service.discover(self.timeout)
+                for addr, name in devices.items():
+                    if name:
+                        logger.info(f"BLE - Name: {name}, mac: {addr}")
+                        HaraldsBTScanner.store_scanlog({"name": name, "addr": addr})
+            except Exception as e:
+                logger.warn(f"Error retrieving ble Bluetooth device info: {e}")
+                time.sleep(2)
+
+
+    @staticmethod
+    def store_scanlog(dev):
+        with open('scanlog.txt', 'a') as f:
+            logger.debug(f"POST device {dev['name']}")
+
+            # HACK BLACKLIST
+            if re.search('Forerunner|JBL|bash', dev['name']):
+                logger.warning(f"IGNORE DEVICE {dev['name']}")
+
+            elif dev['name']:
+                name = dev['name'].replace('’', "'")
+                data = '{"name": "' + f"{name}" + '", ' + \
+                       '"mac": "'  + f"{dev['addr']}" + '"}\n'
+                f.write(data)
+                # ANOTHER DIRTY HACK AGAINST EMOJIS >:[
+                if not re.match("^[a-zA-Z0-9 _-’]*$", name):
+                    logger.warning(f"IGNORE DEVICE {dev['name']}")
+                else:
+                    try:
+                        requests.post(
+                             HARALDS_FLASK_SERVER + "/haralds-scan",
+                             json=data,
+                             timeout=5.0)
+                    except Exception as err:
+                        logger.warning(f"{err}")
+
+
+if __name__ == '__main__':
+    # options
+    parser = OptionParser()
+    parser.add_option(
+        "-t", "--timeout", dest="timeout", action="store", default=10)
+    parser.add_option(
+        "-d", "--debug", dest="debug", action="store_true", default=False)
+
+    (options, args) = parser.parse_args()
+
+    logging.basicConfig(
+        #filename='/dev/null',
+        encoding="utf-8",
+        level=logging.DEBUG if options.debug else logging.INFO,
+        format="%(asctime)s %(message)s",
+        datefmt="%d.%m.%Y %H:%M:%S")
+
+    scanner = HaraldsBTScanner(int(options.timeout))
+    scanner.start()
+
+
